b7f627f68274fb460554424a3976b2f1689a65d8
jihod <jihodot@gmail.com>
2020-06-14 16:09:15 +0900

    [feat] styled components example
    example used case

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const GlobalStyle = createGlobalStyle `
+  body{
+    padding:0;
+    margin:0;
+  }
+`;
+
+
+const Card = styled.div`
+  background-color: red;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
+
+const Form = () => (
+  <Card>
+    <Button>Hello</Button>
+  </Card>
+);
+class App extends Component {
+  render(){
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
+    );
+  }
 }
+
+
+
+const Container = styled.div`
+  height:100vh;
+  width:100%;
+  background-color:pink;
+  ${Card}:{
+    background-clor:blue;
+  }
+`;
+
+
+
 
 export default App;
